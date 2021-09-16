7f47c4d179672f0119edb39d7f08633ca126ebb6
DIBAL7 <si.rincon@uniandes.edu.co>
2021-09-15 20:55:20 -0500

    Corrección conflictos

--- App/view.py
+++ App/view.py
@@ -86,13 +86,7 @@ while True:
             catalog = initCatalog('LINKED_LIST')
             loadData(catalog)
         print("Cargando información de los archivos ....")
-<<<<<<< HEAD
-        catalog = initCatalog()
-        loadData(catalog)
-        print('Obras cargadas: ' + str(lt.size(catalog['artworks'])))
-=======
         print('Obras cargados: ' + str(lt.size(catalog['artworks'])))
->>>>>>> 2c12797e7cf0910f8107057ec341a70e228f1b5f
         print('Artistas cargados: ' + str(lt.size(catalog['artists'])))
     
     elif int(inputs[0]) == 2:
